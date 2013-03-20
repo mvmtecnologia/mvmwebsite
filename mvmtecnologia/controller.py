b03f6b3df050d1db91656be3fc5b3c55294f3579
soliva <soliva@soliva-PC>
2013-03-19 21:59:50 -0300

    alteração

--- mvmtecnologia/controller.py
+++ mvmtecnologia/controller.py
@@ -4,29 +4,33 @@ from google.appengine.ext.webapp import template
 from google.appengine.ext.webapp._webapp25 import RequestHandler
 from google.appengine.ext.webapp.util import run_wsgi_app
 from model import Contato
-import datetime
-
-class HomePage(RequestHandler):
+import datetime   
+import logging
+ 
+class HomeHandler(RequestHandler):
     def get(self):
         self.response.out.write(template.render('pages/home.html', {}))
 
+   
+
+class ContatoHandler(RequestHandler):
+    
     def post(self):
             contato = Contato(nome=self.request.get('inputName'), email=self.request.get('inputEmail'),
-                              message=self.request.get('comments'), dataContato=datetime.datetime.now().date())
+                              mensagem=self.request.get('comments'), dataContato=datetime.datetime.now().date())
+            
             if contato.isValid():
-                contato.save() 
-            
+                contato.put() 
+                contato.enviarEmail()
+                 
             return  self.redirect('/')
-            # enviar email
-            
-            # message=  mail.EmailMessage('mvmtecnologia@appspot.gserviceaccount.com', to='mvmtecnologia@gmail.com',
-            # subject=self.request.get('nome'), body=self.request.get('message'))
-            # message.Send()
+             
+           
         
 
 application = webapp.WSGIApplication(
-                                     [('/', HomePage),
-                                      ],
+                                     [('/', HomeHandler),
+                                      ('/contato', ContatoHandler),],
                                      debug=True)
 
 def main():
--- mvmtecnologia/model.py
+++ mvmtecnologia/model.py
@@ -1,20 +1,36 @@
 '''
 Created on 12/01/2013
-
+@author: matheus cardoso
 @author: soliva
 '''
+from google.appengine.api import mail
 from google.appengine.ext import db
+import logging
 
 class Contato(db.Model):
+    '''
+    classe responsavel em representar o
+    contato feito pelo cliente
+    '''
     nome = db.StringProperty()
     email = db.EmailProperty()
-    message = db.TextProperty()
+    mensagem = db.TextProperty()
     dataContato = db.DateProperty()
 
+    
     def isValid(self):
-        if self.nome and self.email and self.message and self.dataContato:
+        if self.nome and self.email and self.mensagem and self.dataContato:
             return True
         else: False
-        
-
-
+     
+    
+    def enviarEmail(self):
+            logging.info('enviando email')
+            mail.send_mail(sender="mvmtecnologia@gmail.com",
+            to="solivavinicius@gmail.com>",
+            subject="Contato MVMTecnologia",
+            body=self.mensagem)    
+            
+            
+            
+            
