de985d1709b5db13ca34c5194f94c5f8eb0a81c5
soliva <soliva@soliva-PC>
2013-03-21 00:23:19 -0300

    validação no formulario de contato

--- mvmtecnologia/controller.py
+++ mvmtecnologia/controller.py
@@ -5,6 +5,21 @@ from google.appengine.ext.webapp._webapp25 import RequestHandler
 from google.appengine.ext.webapp.util import run_wsgi_app
 from model import Contato
 import datetime   
+"""
+ Copyright (C) 2013 MVM Tecnologia, Inc.
+
+ Licensed under the Apache License, Version 2.0 (the "License");
+ you may not use this file except in compliance with the License.
+  You may obtain a copy of the License at
+ 
+  http://www.apache.org/licenses/LICENSE-2.0
+ 
+  Unless required by applicable law or agreed to in writing, software
+  distributed under the License is distributed on an "AS IS" BASIS,
+  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+  See the License for the specific language governing permissions and
+  limitations under the License.
+ """
  
 class HomeHandler(RequestHandler):
     def get(self):
